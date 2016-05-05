02cb1f332d3af3082e6352448b5a9045a9ad319f
nnsnodnb <ahr63_gej@me.com>
2016-05-05 19:05:18 +0900

    Initial commit

---
+++ plugins/daily.py
@@ -0,0 +1,58 @@
+from slacker import Slacker
+import sys, os, time, json, datetime, requests
+sys.path.append(os.path.dirname(os.path.abspath(__file__)) + '/../')
+import slackbot_settings
+
+def getJson(url, parameters):
+	r = requests.get(url, params = parameters)
+	data = json.loads(r.text)
+
+	return data
+
+def muscleCounter(json):
+	counter = 0
+	for i in range(len(json)):
+		if json[i]['name'] == 'muscle':
+			counter = json[i]['count']
+	return counter
+
+def muscle(now):
+	yesterday = now + datetime.timedelta(days = -1)
+	muscleCount = 0
+
+	url = 'https://slack.com/api/channels.list'
+	parameters = {'token' : slackbot_settings.API_TOKEN}
+	json_2 = getJson(url, parameters)
+
+	for i in range(len(json_2['channels'])):
+		channelId = json_2['channels'][i]['id']
+		parameters['channel'] = channelId
+		url = 'https://slack.com/api/channels.history'
+		json = getJson(url, parameters)
+
+		for j in range(len(json['messages'])):
+			messagePostTime = datetime.datetime.fromtimestamp(float(json['messages'][j]['ts']))
+			# Time comparison
+			if messagePostTime >= yesterday:
+				# Muscle Counter
+				if 'reactions' in json['messages'][j]:
+					muscleCount += muscleCounter(json['messages'][j]['reactions'])
+				else:
+					break
+	
+	return muscleCount
+
+def postMessage(count):
+	slack = Slacker(slackbot_settings.API_TOKEN)
+	slack.chat.post_message(
+		'muscle',
+		'寝る前に今日も' + str(count) + '回腹筋してマッスルじゃ:muscle:',
+		as_user=True)
+
+if __name__ == '__main__':
+	while(True):
+		now = datetime.datetime.now().strftime('%H:%M:%S')
+		if now == '21:00:00':
+			muscleCount = muscle(datetime.datetime.now())
+			time.sleep(1)
+			postMessage(muscleCount)
---
+++ run.py
@@ -0,0 +1,12 @@
+from slackbot.bot import Bot
+from slackbot.bot import listen_to
+import slackbot_settings
+
+@listen_to('筋トレ')
+def fight(message):
+	message.reply('その意気じゃ！！!:muscle:')
+	message.react('muscle')
+
+if __name__ == "__main__":
+	bot = Bot()
+	bot.run()
