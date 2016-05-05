1f77cae4832bc4b0429d0666ed471024ba3f09a2
nnsnodnb <ahr63_gej@me.com>
2016-05-05 19:22:59 +0900

    Reaction to muscle message:muscle:

--- run.py
+++ run.py
@@ -7,6 +7,11 @@ def fight(message):
 	message.reply('その意気じゃ！！!:muscle:')
 	message.react('muscle')
 
+@listen_to('muscle')
+def muscle(message):
+	message.send(':muscle::muscle:')
+	message.react('muscle')
+
 if __name__ == "__main__":
 	bot = Bot()
 	bot.run()
